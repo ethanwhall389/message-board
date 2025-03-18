552722858cee8a72f199788249b07eeff90c496a
Ethan Hall <ethanwhall@icloud.com>
2025-03-18 08:16:35 -0400

    Add newMessageRouter, 404 page

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = 3000;
 
 const indexRouter = require('./routes/indexRouter');
+const newMessageRouter = require('./routes/newMessageRouter');
 
 app.use('/', indexRouter);
+app.use('/new', newMessageRouter);
+app.use('*', (req, res) => res.send('Page not found'));
 
 app.listen(PORT);
