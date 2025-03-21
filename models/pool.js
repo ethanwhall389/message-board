91c4345652eaa2f38f15dffff322788dd1e766ab
Ethan Hall <ethanwhall@icloud.com>
2025-03-21 18:35:45 -0400

    Add DATABASE_URL log

--- models/pool.js
+++ models/pool.js
@@ -1,6 +1,10 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+console.log("Database connection settings:", {
+  connectionString: process.env.DATABASE_URL,
+});
+
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }, // Required for Railway
