3a8fcd5e7982bf23232f9b0b005551d61d8224a9
Ethan Hall <ethanwhall@icloud.com>
2025-03-21 18:18:55 -0400

    Fix(indexController) add await before redirect

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,34 +1,34 @@
-const db = require('../models/db');
-const queries = require('../models/queries');
-const asyncHandler = require('express-async-handler');
-const {format, parseISO} = require('date-fns');
+const db = require("../models/db");
+const queries = require("../models/queries");
+const asyncHandler = require("express-async-handler");
+const { format, parseISO } = require("date-fns");
 
 const getAllMessages = asyncHandler(async (req, res) => {
-    const messages = await queries.getAllMessages();
+  const messages = await queries.getAllMessages();
 
-    const formattedMessages = messages.map((message) => {
-        const formattedDate = format(message.added, 'MMM dd, yyyy');
-        return {
-            id: message.id,
-            text: message.text,
-            user: message.user,
-            added: format(message.added, 'MMM dd, yyyy'),
-        }
-    }) 
+  const formattedMessages = messages.map((message) => {
+    const formattedDate = format(message.added, "MMM dd, yyyy");
+    return {
+      id: message.id,
+      text: message.text,
+      user: message.user,
+      added: format(message.added, "MMM dd, yyyy"),
+    };
+  });
 
-    if (!formattedMessages) {
-        throw new Error('Messages not found');
-    }
-    res.render('index', {messages: formattedMessages});
-})
+  if (!formattedMessages) {
+    throw new Error("Messages not found");
+  }
+  res.render("index", { messages: formattedMessages });
+});
 
 const addNewMessage = asyncHandler(async (req, res) => {
-    const {user, message} = req.body;
-    queries.addNewMessage(user, message);
-    res.redirect('/');
-})
+  const { user, message } = req.body;
+  await queries.addNewMessage(user, message);
+  res.redirect("/");
+});
 
 module.exports = {
-    getAllMessages,
-    addNewMessage,
-}
+  getAllMessages,
+  addNewMessage,
+};
--- models/queries.js
+++ models/queries.js
@@ -1,34 +1,32 @@
-const pool = require('./pool');
+const pool = require("./pool");
 
 async function getAllMessages() {
-    const query = {
-        text: 'SELECT * FROM messages',
-    }
-    const {rows} = await pool.query(query);
-    console.log('get all messages: rows: ');
-    console.log(rows);
-    return rows;
+  const query = {
+    text: "SELECT * FROM messages",
+  };
+  const { rows } = await pool.query(query);
+  return rows;
 }
 
 async function getMessageById(id) {
-    const query = {
-        text: 'SELECT * FROM messages WHERE id=$1',
-        values: [id],
-    }
-    const {rows} = await pool.query(query);
-    return rows;
+  const query = {
+    text: "SELECT * FROM messages WHERE id=$1",
+    values: [id],
+  };
+  const { rows } = await pool.query(query);
+  return rows;
 }
 
 async function addNewMessage(user, message) {
-    const query = {
-        text: 'INSERT INTO messages ("text", "user") VALUES ($1, $2)',
-        values: [message, user],
-    }
-    await pool.query(query);
+  const query = {
+    text: 'INSERT INTO messages ("text", "user") VALUES ($1, $2)',
+    values: [message, user],
+  };
+  await pool.query(query);
 }
 
 module.exports = {
-    getAllMessages,
-    getMessageById,
-    addNewMessage,
-}
+  getAllMessages,
+  getMessageById,
+  addNewMessage,
+};
